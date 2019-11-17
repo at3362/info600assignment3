72b86b9c34cbfd2c054bd4842b8bb695eb04b4e0
unknown <at3362@drexel.edu>
2019-11-17 18:33:42 -0500

    INFO600 Assignment 3 - Trinidad

--- js/main.js
+++ js/main.js
@@ -1,26 +1,97 @@
 document.addEventListener('DOMContentLoaded', assignClickHandler)
 
 function assignClickHandler () {
-  document.getElementById('addRec').addEventListener('click', function () {
-    const startYear = document.getElementById('startYear').value
-    if (startYear <= 2000) {
+
+  $(document).ready(function(){
+    $("#loadData").on("click", function(){
+
+      $("#enteredRecords").empty();
+      $.ajax({
+        method: 'GET',
+        url: '/users',
+        datatype: 'json',
+        type: 'get',
+        cache: false,
+        success: function(data){
+          $(data.records).each(function(index,value){
+              const id = value.id;
+              const date = new Date()
+              const time = date.getHours() + ':' + date.getMinutes()
+              $('#enteredRecords').append(time +' - '+ value.fullName+', '+' '+value.major+', '+value.startYear+
+                '<button value="'+value.id+'" id="delRec">Delete</button><br>');
+          });
+        }
+
+      });
+         
+    });
+  });
+
+  $(document).ready(function(){
+    $("#addRec").on("click", function(){
+      const startYear = document.getElementById('startYear').value
+      if (startYear < 2000) {
       window.alert('Incorrect year: ' + startYear)
       return
-    }
-    const fullName = document.getElementById('fullName').value
-    const major = document.getElementById('major').value
+      }
+      $.ajax({
+        method: 'POST',
+        url: '/users/',
+        type: 'post',
+        cache: false,
+        data: {
+          fullName: $('#fullName').val(),
+          major: $('#major').val(),
+          startYear: $('#startYear').val()
 
-    const date = new Date()
-    const time = date.getHours() + ':' + date.getMinutes()
+        }
 
-    const newEntry = time + ' - ' + fullName + ', ' + major + ', ' + startYear
+      })
+      alert("Record added.");
+      document.getElementById('inputs').reset()
+      
+    });
 
-    const enteredRecords = document.getElementById('enteredRecords')
-    let newChild = document.createElement('li')
-    newChild.appendChild(document.createTextNode(newEntry))
+  });
 
-    enteredRecords.appendChild(newChild)
+  $(document).on("click","#delRec", function(){
 
-    document.getElementById('inputs').reset()
-  })
+   const id = $(this).val();
+   //const fullname = $(this).val();
+       
+                  $.ajax({
+                    url: '/user/'+id,
+                    method: 'DELETE',
+                    success: function(){
+                      alert("The record with id " + id + " has been deleted");
+                      reload();
+                    },
+                    error: function(error){
+                      alert(error);
+                    }
+                  });
+                
+            })
+               
+  function reload(){
+    $("#enteredRecords").empty();
+      $.ajax({
+        method: 'GET',
+        url: '/users',
+        datatype: 'json',
+        type: 'get',
+        cache: false,
+        success: function(data){
+          $(data.records).each(function(index,value){
+              const id = value.id;
+              const date = new Date()
+              const time = date.getHours() + ':' + date.getMinutes()
+              $('#enteredRecords').append(time +' - '+ value.fullName+', '+' '+value.major+', '+value.startYear+
+                '<button value="'+value.id+'" id="delRec">Delete</button><br>');
+          });
+        }
+
+      });
+  }
+
 }
